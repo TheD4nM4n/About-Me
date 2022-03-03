320b8c5bb22e7512ab8f077d60e49402007545d3
Daniel Poe <thed4nm4n@gmail.com>
2022-03-03 11:30:41 -0500

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Daniel Jaye Poe (Student) on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by Daniel Jaye Poe (Student) on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
