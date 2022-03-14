dbe2f2fee30fdc03994b4871c634fb5a834452c1
Daniel Poe <thed4nm4n@gmail.com>
2022-03-14 12:08:31 -0400

    added back n fourth

--- About Me/MusicView.swift
+++ About Me/MusicView.swift
@@ -20,6 +20,8 @@ struct MusicView: View {
                 CardView(image: Image("hindrance"), title: "hindrance. (feat. Atlas in Motion)", description: "astra", year: "2022", url: "https://youtu.be/DK4kV7ER-p0")
                 
                 CardView(image: Image("umeanevenlesstomenow"), title: "u mean even less to me now", description: "lieu", year: "2020", url: "https://youtu.be/OejyzWze9DI")
+                
+                CardView(image: Image("backnfourth"), title: "back n fourth", description: "ericdoa", year: "2021", url: "https://youtu.be/M5yblXlUk9M")
             }
         }
     }
